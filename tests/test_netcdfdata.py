2ddf9a5d1b577fff47188160048ffa384b35c275
rwegener <rwegener2@yahoo.com>
2019-01-06 11:49:32 -0700

    fixing testing errors

--- tests/test_netcdfdata.py
+++ tests/test_netcdfdata.py
@@ -1,6 +1,7 @@
 from wnpmonsoon.netcdfdata import NetcdfData
 from numpy.testing import assert_almost_equal
 import netCDF4 as nc
+import numpy as np
 import pytest
 
 
@@ -40,49 +41,49 @@ def vas_cnrmcm5(path_vas_cnrmcm5):
     return NetcdfData(path_vas_cnrmcm5)
 
 
-def test_init_pr_access10(path_pr_access10):
+def test_init_pr_access10(path_pr_access10, direct_pr_access10):
     nc_obj = NetcdfData(path_pr_access10)
     assert nc_obj.var_name == 'pr'
     assert nc_obj.model_id == 'ACCESS1-0'
     assert nc_obj.var_units == 'kg m-2 s-1'
-    assert_almost_equal(nc_obj.variable, direct_pr_access10.variables['pr'], decimal=1)
-    assert_almost_equal(nc_obj.lats, direct_pr_access10.variables['lat'], decimal=1)
-    assert_almost_equal(nc_obj.lons, direct_pr_access10.variables['lon'], decimal=1)
-    assert_almost_equal(nc_obj.time, direct_pr_access10.variables['time'], decimal=1)
+    assert_almost_equal(nc_obj.variable, direct_pr_access10.variables['pr'], decimal=2)
+    assert_almost_equal(nc_obj.lats, direct_pr_access10.variables['lat'], decimal=2)
+    assert_almost_equal(nc_obj.lons, direct_pr_access10.variables['lon'], decimal=2)
+    assert_almost_equal(nc_obj.time, direct_pr_access10.variables['time'], decimal=2)
     assert nc_obj.calendar == 'proleptic_gregorian'
     assert nc_obj.t_units == 'days since 0001-01-01'
 
 
-def test_init_uas_cmcccm(path_uas_cmcccm):
+def test_init_uas_cmcccm(path_uas_cmcccm, direct_uas_cmcccm):
     nc_obj = NetcdfData(path_uas_cmcccm)
     assert nc_obj.var_name == 'uas'
     assert nc_obj.model_id == 'CMCC-CM'
     assert nc_obj.var_units == 'm s-1'
-    assert_almost_equal(nc_obj.variable, path_uas_cmcccm.variables['pr'], decimal=1)
-    assert_almost_equal(nc_obj.lats, path_uas_cmcccm.variables['lat'], decimal=1)
-    assert_almost_equal(nc_obj.lons, path_uas_cmcccm.variables['lon'], decimal=1)
-    assert_almost_equal(nc_obj.time, path_uas_cmcccm.variables['time'], decimal=1)
+    assert_almost_equal(nc_obj.variable, direct_uas_cmcccm.variables['uas'], decimal=2)
+    assert_almost_equal(nc_obj.lats, direct_uas_cmcccm.variables['lat'], decimal=2)
+    assert_almost_equal(nc_obj.lons, direct_uas_cmcccm.variables['lon'], decimal=2)
+    assert_almost_equal(nc_obj.time, direct_uas_cmcccm.variables['time'], decimal=2)
     assert nc_obj.calendar == 'standard'
     assert nc_obj.t_units == 'days since 2006-1-1'
 
 
-def test_init_vas_cnrmcm5(path_vas_cnrmcm5):
+def test_init_vas_cnrmcm5(path_vas_cnrmcm5, direct_vas_cnrmcm5):
     nc_obj = NetcdfData(path_vas_cnrmcm5)
     assert nc_obj.var_name == 'vas'
     assert nc_obj.model_id == 'CNRM-CM5'
     assert nc_obj.var_units == 'm s-1'
-    assert_almost_equal(nc_obj.variable, path_vas_cnrmcm5.variables['pr'], decimal=1)
-    assert_almost_equal(nc_obj.lats, path_vas_cnrmcm5.variables['lat'], decimal=1)
-    assert_almost_equal(nc_obj.lons, path_vas_cnrmcm5.variables['lon'], decimal=1)
-    assert_almost_equal(nc_obj.time, path_vas_cnrmcm5.variables['time'], decimal=1)
+    assert_almost_equal(nc_obj.variable, direct_vas_cnrmcm5.variables['vas'], decimal=2)
+    assert_almost_equal(nc_obj.lats, direct_vas_cnrmcm5.variables['lat'], decimal=2)
+    assert_almost_equal(nc_obj.lons, direct_vas_cnrmcm5.variables['lon'], decimal=2)
+    assert_almost_equal(nc_obj.time, direct_vas_cnrmcm5.variables['time'], decimal=2)
     assert nc_obj.calendar == 'gregorian'
     assert nc_obj.t_units == 'days since 2006-01-01 00:00:00'
 
 
 def test_pr_unit_conversion(pr_access10, direct_pr_access10):
-    unit_converted = pr_access10.pr_unit_conversion()
-    assert unit_converted.var_units == 'mm hr-1'
-    assert_almost_equal(unit_converted.variable, direct_pr_access10.variables['pr']*86400, decimal=1)
+    pr_access10.pr_unit_conversion()
+    assert pr_access10.var_units == 'mm hr-1'
+    assert_almost_equal(pr_access10.variable, np.asarray(direct_pr_access10.variables['pr'])*86400, decimal=2)
 
 
 def test_pr_units_input_error(uas_cmcccm):
