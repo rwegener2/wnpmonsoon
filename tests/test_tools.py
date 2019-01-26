77020d425a4d13ea54e10b828969d11c0691bff8
rwegener2 <rwegener2@yahoo.com>
2019-01-26 12:22:10 -0700

    tools naming refactor

---
+++ tests/test_tools.py
@@ -0,0 +1,60 @@
+from wnpmonsoon.tools import degfromnorth
+import numpy as np
+
+
+def test_30_deg():
+    assert degfromnorth(np.sqrt(3), 1) == 240
+
+
+def test_45_deg():
+    assert degfromnorth(1, 1) == 225
+
+
+def test_60_deg():
+    assert degfromnorth(1, np.sqrt(3)) == 210
+
+
+def test_90_deg():
+    assert degfromnorth(0, 1) == 180
+
+
+def test_120_deg():
+    assert degfromnorth(-1, np.sqrt(3)) == 150
+
+
+def test_135_deg():
+    assert degfromnorth(1, -1) == 315
+
+
+def test_150_deg():
+    assert degfromnorth(-np.sqrt(3), 1) == 120
+
+
+def test_180_deg():
+    assert degfromnorth(-1, 0) == 90
+
+
+def test_210_deg():
+    assert degfromnorth(-np.sqrt(3), -1) == 60
+
+
+def test_225_deg():
+    assert degfromnorth(-1, -1) == 45
+
+
+def test_240_deg():
+    assert degfromnorth(-1, -np.sqrt(3)) == 30
+
+
+def test_315_deg():
+    assert degfromnorth(1, -1) == 315
+
+
+def test_330_deg():
+    assert degfromnorth(np.sqrt(3), -1) == 300
+
+
+def test_all_as_array():
+    uas = np.array([1, 1, 0, -np.sqrt(3), -1, np.sqrt(3), -1, -np.sqrt(3), -1, 1, -1, np.sqrt(3)])
+    vas = np.array([1, np.sqrt(3), 1, 1, -1, -1, 0, -1, np.sqrt(3), -1, 1, -1])
+    assert (degfromnorth(uas, vas) == np.array([225, 210, 180, 120, 45, 300, 90, 60, 150, 315, 135, 300])).all()
---
+++ wnpmonsoon/tools.py
@@ -0,0 +1,36 @@
+# from wnpmonsoon.ncdata import NCdata
+import numpy as np
+
+
+def degfromnorth(uas, vas):
+    """
+    :param uas: np array of eastward wind speed - strength of wind coming from the east
+    :param vas: np array of northward wind speed - strength of wind coming from the north
+    :return: wind direction - wind coming from that direction in degrees clockwise from north
+    """
+    # Set values where both uas and vas are equal to zero to nan, so that no wind corresponds to nan
+    both_zero = np.logical_and(uas == 0, vas == 0)
+    zeros_indices = zip(*np.where(both_zero is True))
+    uas_nans = uas
+    vas_nans = vas
+    for i, j, k in zeros_indices:
+        uas_nans[i, j, k] = np.NaN
+        vas_nans[i, j, k] = np.NaN
+    # Calculate the degrees ccw from west using arctan2, which accounts for when arctan blows up
+    deg_west = np.degrees(np.arctan2(vas_nans, uas_nans))
+    # Return the result as the degrees clockwise from north
+    return (-deg_west + 270) % 360
+
+
+# def wind_direction(uas_file, vas_file, save_location):
+#     """
+#     :param uas_file: filepath of 'uas'
+#     :param vas_file: filepath of 'vas'
+#     :param save_location: where to save output
+#     :return: a NCdata object wind direction
+#     """
+#     uas = NCdata(uas_file)
+#     vas = NCdata(vas_file)
+#     wind_dir_data = degfromnorth(uas.variable, vas.variable)
+#     uas.write(save_location, variable=wind_dir_data, var_name='wdir', var_units='degrees clockwise from north')
+#     return NCdata(save_location)
